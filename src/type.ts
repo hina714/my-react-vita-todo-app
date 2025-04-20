3f1fb783e449b7e084048320f2ed60792582cf61
hina714 <aok06242006@gmail.com>
2025-04-20 22:47:20 +0900

    それぞれのタスク件数を画面上に表示する

--- src/type.ts
+++ src/type.ts
@@ -10,4 +10,9 @@ export type TodoItemType = {
     // 完了／未完了タスクが0件の場合は、該当セクションを非表示にする
     isHiddenCompleted?: boolean,
     isHiddenUncompleted?: boolean,
-}
+}   //全体・未完了・完了それぞれのタスク件数を画面上に表示する
+export type TodoCountType = {
+    all: number,
+    uncompleted: number,
+    completed: number,
+}   //全体・未完了・完了それぞれのタスク件数を画面上に表示する
